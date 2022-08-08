bfd7381ad17fad33080f9bd9734c0e8d05bf232e
Oluwatobi Amoniyan <102077396+Oluwatoby8965@users.noreply.github.com>
2022-08-08 23:21:41 +0100

    printf

---
+++ custom_functions.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <unistd.h>
+
+
+/**
+* _putchar - writes the character c to stdout
+* @c: The character to print
+*
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+/**
+*_strlen - reset number
+*Description: This function return a length for some string
+*@s: pointer char
+*Return: int length
+*/
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s++)
+	{
+		len++;
+	}
+	return (len);
+}
+/**
+*_puts - print string
+*Description: print some string
+*@str: pointer char
+*Return: Nothing
+*/
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; i < _strlen(str); i++)
+	{
+		_putchar(str[i]);
+	}
+}
